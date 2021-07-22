2bbaca5507095eb7e02f2e3eb88007327e36f30d
Piotr Dobiech <pitcer@protonmail.com>
2021-07-22 10:41:59 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm") version "1.5.21"
+    id("org.jetbrains.intellij") version "1.1.3"
+}
+
+group = "com.github.pitcer.partialscroll"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+}
+
+intellij {
+    version.set("2021.1")
+}
